54b63b30452ce2c30f1113ed69f5642309d0f52e
vikash <vp3637637@gmail.com>
2024-05-09 18:01:25 +0530

    contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './Style.scss'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <div className="map">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d7234.373524746348!2d84.01825787740104!3d24.959759663586848!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1712196019295!5m2!1sen!2sin" width="100%" height="300" style={{border:0}} allowFullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+      <form action="https://formspree.io/f/xleqvebl" method='post'>
+        <input type="text" placeholder='username'/>
+        <input type="email" placeholder='Email'/>
+        <textarea cols='35' rows='4'></textarea>
+      </form>
+    </div>
   )
 }
 
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,6 +3,7 @@ import './Style.scss'
 import ecom from '../../assets/ecom.png'
 import chat from '../../assets/chat.png'
 import movie from '../../assets/movie.png'
+import {FaGithub, FaShare } from "react-icons/fa";
 
 const Projects = () => {
   return (
@@ -12,9 +13,54 @@ const Projects = () => {
       <section>
         <div className="project project1">
           <img src={movie} alt="" />
-          <div className="content"></div>
-          {/* 999999999999999999999999 */}
+          <div className="content">
+            <h3>Movie app🎬</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit aperiam aut magnam nostrum exercitationem. Labore blanditiis iste eos dignissimos consequatur veritatis exercitationem? Cum reiciendis voluptatibus nihil error odio animi perspiciatis.</p>
+            <div className="tech flex gap-5">
+              <b>React</b>
+              <b>Redux</b>
+              <b>Scss</b>
+            </div>
+            <div className="options">
+              <p>Code <FaGithub/></p>
+              <p>Live demo <FaShare/></p>
+            </div>
+          </div>
         </div>
+          {/* 999999999999999999999999 */}
+        <div className="project project2">
+          <img src={ecom} alt="" />
+          <div className="content">
+            <h3>DevShops🏪</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit aperiam aut magnam nostrum exercitationem. Labore blanditiis iste eos dignissimos consequatur veritatis exercitationem? Cum reiciendis voluptatibus nihil error odio animi perspiciatis.</p>
+            <div className="tech flex gap-5">
+              <b>React</b>
+              <b>Tailwind</b>
+            </div>
+            <div className="options">
+              <p>Code <FaGithub/></p>
+              <p>Live demo <FaShare/></p>
+            </div>
+          </div>
+        </div>
+          {/* 999999999999999999999999 */}
+        <div className="project project3">
+          <img src={chat} alt="" />
+          <div className="content">
+            <h3>Chat app📲</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit aperiam aut magnam nostrum exercitationem. Labore blanditiis iste eos dignissimos consequatur veritatis exercitationem? Cum reiciendis voluptatibus nihil error odio animi perspiciatis.</p>
+            <div className="tech flex gap-5">
+              <b>React</b>
+              <b>Redux</b>
+              <b>Scss</b>
+            </div>
+            <div className="options">
+              <p>Code <FaGithub/></p>
+              <p>Live demo <FaShare/></p>
+            </div>
+          </div>
+        </div>
+          {/* 999999999999999999999999 */}
       </section>
     </div>
   )
